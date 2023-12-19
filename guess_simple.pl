dc41ad4f9f60245b2737a0284738818272566d2c
Atiman <153884643+atiman-rohatgi@users.noreply.github.com>
2023-12-18 18:22:00 -0800

    Add files via upload

---
+++ guess_simple.pl
@@ -0,0 +1,26 @@
+:- use_module(library(random)).
+
+%starting the game
+guess :-
+    random(1, 11, RandomNum),  %generating a random number between 1 and 10
+    write('Guess a number between 1 and 10.'), nl,
+    guess_loop(RandomNum).
+
+%the main loop
+guess_loop(RandomNum) :-
+    read(Guess), %reading user's input
+    (   number(Guess) ->
+        check_guess(Guess, RandomNum)
+    ;   write('Please enter a valid number.'), nl,
+        guess_loop(RandomNum)
+    ).
+
+%checking the guess
+check_guess(Guess, RandomNum) :-
+    Guess < RandomNum ->
+    write('Higher.'), nl,
+    guess_loop(RandomNum);
+    Guess > RandomNum ->
+    write('Lower.'), nl,
+    guess_loop(RandomNum);
+    write('Correct!'), nl.
